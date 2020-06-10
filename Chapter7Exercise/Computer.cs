16031ab7e2e603573d69ea77c780a065575644b5
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-10 11:57:47 -0400

    Add project files.

---
+++ Chapter7Exercise/Computer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Technology
+{
+    public class Computer
+    {       
+
+        public bool Power { get; set; } = false;
+        public bool Use { get; set; } = false;
+        public double Memory { get; set; }
+        public string Brand { get; } = "Hackintosh";
+
+        public Computer(bool power, bool use, double memory, string brand)
+        {
+            Power = power;
+            Use = use;
+            Memory = memory;
+            Brand = brand;
+        }
+
+        public void Sleep()
+        {
+            Power = true;
+            Use = false;
+        }
+
+        public string PowerOn()
+        {
+            if (!Power)
+            {
+                Use = false;
+            }
+            else if (Use == true) 
+            {
+                Power = true;
+            }
+            
+            return "System Powering on!";
+        }
+
+
+
+
+    }
+}
---
+++ Chapter7Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Technology
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer test = new Computer(false, false, 16, "Dell");
+
+            Console.WriteLine($"new computer {test}");
+        }
+    }
+}
